7ac0d8b9ac9beaa57f7ecdfd978f1ae47aad64a2
xiaoyu <171222853@qq.com>
2024-12-30 17:11:28 +0800

    远程获取插件GroupIdS

---
+++ wechat-ferry-mvn/src/test/java/com/wechat/ferry/plugin/PluginLoaderTest.java
@@ -0,0 +1,11 @@
+package com.wechat.ferry.plugin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PluginLoaderTest {
+    @org.junit.jupiter.api.Test
+    void loadPlugins() {
+        PluginLoader.getRemotePluginGroupIdS();
+    }
+
+}
